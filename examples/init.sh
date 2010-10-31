211da36bb147dfba60ee654dcc35cdc2ffe01d63
Jan Gasthaus <j.gasthaus@ucl.ac.uk>
2010-10-31 17:42:51 +0100

    Make build system more robust.

--- examples/init.sh
+++ examples/init.sh
@@ -1,2 +1,3 @@
 export PYTHONPATH=${PYTHONPATH}:`pwd`/../bindings/python
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:`pwd`/../src/.libs
+export DYLD_LIBRARY_PATH=${DYLD_LIBRARY_PATH}:`pwd`/../src/.libs
