9fccba0b70c8322a16c037c0f1e71e7f41c78b03
Jan Gasthaus <jgasthaus@googlemail.com>
2016-05-20 15:49:36 +0200

    Update LICENSE to Apache 2.0

--- examples/init.sh
+++ examples/init.sh
@@ -1,3 +1,3 @@
-export PYTHONPATH=${PYTHONPATH}:`pwd`/../bindings/python
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:`pwd`/../src/.libs
-export DYLD_LIBRARY_PATH=${DYLD_LIBRARY_PATH}:`pwd`/../src/.libs
+export PYTHONPATH=${PYTHONPATH}:`pwd`/../build/bindings
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:`pwd`/../build/src/libplump
+export DYLD_LIBRARY_PATH=${DYLD_LIBRARY_PATH}:`pwd`/../build/src/libplump
