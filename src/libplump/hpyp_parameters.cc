540e33045f5676d1780584da738b50483df2a9ac
Jan Gasthaus <jgasthaus@googlemail.com>
2016-05-20 16:02:35 +0200

    Updated header comments to reflect license change

--- src/libplump/hpyp_parameters.cc
+++ src/libplump/hpyp_parameters.cc
@@ -1,8 +1,25 @@
+/*
+ * Copyright 2008-2016 Jan Gasthaus
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include <cmath> // for pow
 #include <algorithm> // for fill
 #include "libplump/hpyp_parameters.h"
 #include "libplump/context_tree.h" // for WrappedNodeList
 #include "libplump/utils.h" // for tracer
+
 
 namespace gatsby { namespace libplump {
 
